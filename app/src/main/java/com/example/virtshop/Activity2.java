ae21f6a8b370e2f93b1c2f55c8f42404f420e92c
pinkrain99 <valmi2009@gmail.com>
2020-01-05 17:35:16 +0200

    mainActivity is working adding a ImageButton to a search page

--- app/src/main/java/com/example/virtshop/Activity2.java
+++ app/src/main/java/com/example/virtshop/Activity2.java
@@ -1,4 +1,4 @@
-package com.example.widget;
+package com.example.virtshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/example/virtshop/MainActivity.java
+++ app/src/main/java/com/example/virtshop/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,28 +17,8 @@ import com.hitomi.cmlibrary.CircleMenu;
 import com.hitomi.cmlibrary.OnMenuSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private ImageButton button;
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //this is a comment
-        button = findViewById(R.id.Button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            openActivity2();
-            }
-        });
-
-        }
-
-    CircleMenu circleMenu = findViewById(R.id.circle_menu);
     final String[] arrayName = {
             "sapun",
             "food",
@@ -47,36 +27,48 @@ public class MainActivity extends AppCompatActivity {
             "tv"
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        button = findViewById(R.id.image_button);
+        CircleMenu circleMenu = findViewById(R.id.circle_menu);
 
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
 
-
-        circleMenu.setMainMenu(Color.parseColor( "#CDCDCD"), R.drawable.ic_app, R.drawable.ic_cancel)
+        circleMenu.setMainMenu(Color.parseColor("#CDCDCD"), R.drawable.ic_app, R.drawable.ic_cancel)
                 .addSubMenu(Color.parseColor("#258CFF"), R.drawable.food)
                 .addSubMenu(Color.parseColor("#30A400"), R.drawable.pet)
                 .addSubMenu(Color.parseColor("#FF4B32"), R.drawable.sapun)
                 .addSubMenu(Color.parseColor("#8A39FF"), R.drawable.tv)
                 .addSubMenu(Color.parseColor("#FF6A00"), R.drawable.vege)
                 .setOnMenuSelectedListener(new OnMenuSelectedListener() {
-
                     @Override
                     public void onMenuSelected(int index) {
-                        Toast.makeText(MainActivity.this, "You selected" +arrayName[index], Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "You selected " + arrayName[index], Toast.LENGTH_SHORT).show();
                     }
-
-
-        });
-
-
+                });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menubar) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menubar,menubar);
+        inflater.inflate(R.menu.menubar, menubar);
         return true;
     }
 
-    public void openActivity2(){
-        Intent intent = onNewIntent(this, Activity2.class);
+    public void openActivity2() {
+        Intent intent = new Intent(this, Activity2.class);
         startActivity(intent);
+    }
 }
